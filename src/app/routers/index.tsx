fbd6db128cdb2a1234cd153e6c1dbe1c8697fcbf
aziret <kubaaziret68@gmail.com>
2024-03-05 12:01:52 +0600

    aziret 2008 fihish add router

--- src/app/routers/index.tsx
+++ src/app/routers/index.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from '../../pages/Home/page'
-import AboutClinic from '../../pages/AboutClinic/page'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../../pages/Home/page";
+import AboutClinic from "../../pages/AboutClinic/page";
+import Index from "../../pages/Vakancies/Index";
 
 export default function Routers() {
-	return (
-		<main>
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/about/clinic' element={<AboutClinic />} />
-			</Routes>
-		</main>
-	)
+  return (
+    <main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about/clinic" element={<AboutClinic />} />
+        <Route path="/about/vakancies" element={<Index />} />
+      </Routes>
+    </main>
+  );
 }
---
+++ src/widgets/Partners/Partners.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Partners.css";
+import Partner1 from "./image 28.png";
+import Partner2 from "./image 29.png";
+import Partner3 from "./image 30.png";
+import Partner4 from "./image 31.png";
+import Partner5 from "./image 32.png";
+import Partner6 from "./image 33.png";
+
+const Partners = () => {
+  return (
+    <div id="partners">
+      <div className="container">
+        <div className="partners">
+          <h1>Мы работаем по ДМС</h1>
+          <p>С ведущими страховыми компаниями России</p>
+          <div className="pic-partners">
+            <img src={Partner1} alt="image :/" />
+            <img src={Partner2} alt="image :/" />
+            <img src={Partner3} alt="image :/" />
+            <img src={Partner4} alt="image :/" />
+            <img src={Partner5} alt="image :/" />
+            <img src={Partner6} alt="image :/" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Partners;
