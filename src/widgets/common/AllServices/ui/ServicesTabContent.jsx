45e477beb84ab6513edc55aeaa1066caa9b0192c
Janybek2007 <lr.arek.2007@gmail.com>
2024-03-07 15:31:23 +0600

    updates

--- src/widgets/common/AllServices/ui/ServicesTabContent.jsx
+++ src/widgets/common/AllServices/ui/ServicesTabContent.jsx
@@ -75,7 +75,7 @@ export default function ServicesTabContent({ tab }) {
 				}}
 				spaceBetween={0}
 				slidesPerView={1}
-				className={'swiper block'}
+				className={'swipers block'}
 			>
 				{servicesTabContent.map((item, idx) => (
 					<SwiperSlide key={idx} className={styles.li}>
