a1cbd929b261807b7b3451565d4b2d68bbb4f7dd
aziret <kubaaziret68@gmail.com>
2024-03-07 15:40:04 +0600

    azuret finish sale adaptive

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import Footer from "../widgets/common/FooterEnd/Footer";
@@ -9,18 +8,6 @@ import Header from "../widgets/common/Header";
 import Form2Home from "../widgets/common/form2Home/Form2Home";
 import Routers from "./routers";
 import "./App.css";
-=======
-import { useEffect } from 'react'
-import { useLocation } from 'react-router-dom'
-import Footer from '../widgets/common/FooterEnd/Footer'
-import Footer2 from '../widgets/common/FooterEnd/Footer2'
-import Footer3 from '../widgets/common/FooterEnd/Footer3'
-import Footer4 from '../widgets/common/FooterEnd/Footer4'
-import Header from '../widgets/common/Header'
-import Form2Home from '../widgets/common/form2Home/Form2Home'
-import Routers from './routers'
-import './App.css'
->>>>>>> 1b5acdc4070f9f21695d9e5a5f0bd8b91d77d74f
 function App() {
   const location = useLocation();
 
