807cce0ad68acb8097819f62eae2485e28563d4e
Janybek2007 <lr.arek.2007@gmail.com>
2024-03-12 13:05:16 +0600

    u

--- src/pages/ReviewsPage/components/ReviewTab.jsx
+++ src/pages/ReviewsPage/components/ReviewTab.jsx
@@ -5,54 +5,54 @@ import { Link } from 'react-router-dom'
 import styles from '../page.module.scss'
 
 export default function ReviewTab({ reviewPageIndex }) {
-	async function onClick(index) {
-		gsap.to(window, {
-			duration: 0.5,
-			scrollTo: { y: '#section' + index, offsetY: index === 1 ? 110 : 80 },
-			ease: 'expo'
-		})
-	}
+  async function onClick(index) {
+    gsap.to(window, {
+      duration: 0.5,
+      scrollTo: { y: '#section' + index, offsetY: index === 1 ? 110 : 80 },
+      ease: 'expo',
+    })
+  }
 
-	return (
-		<div className={styles['review-tab']}>
-			<div className='container'>
-				<Link
-					to={'/reviews'}
-					className={clsx({
-						[styles.actives]: !reviewPageIndex
-					})}
-					onClick={() => onClick(1)}
-				>
-					Независимый рейтинг
-				</Link>
-				<Link
-					className={clsx({
-						[styles.actives]: +reviewPageIndex === 2
-					})}
-					to='/reviews?tabIndex=2'
-					onClick={() => onClick(2)}
-				>
-					Интервью с пациентами
-				</Link>
-				<Link
-					className={clsx({
-						[styles.actives]: +reviewPageIndex === 3
-					})}
-					to='/reviews?tabIndex=3'
-					onClick={() => onClick(3)}
-				>
-					Отзывы в социальных сетях
-				</Link>
-				<Link
-					className={clsx({
-						[styles.actives]: +reviewPageIndex === 4
-					})}
-					to='/reviews?tabIndex=4'
-					onClick={() => onClick(4)}
-				>
-					Отзывы пациентов
-				</Link>
-			</div>
-		</div>
-	)
+  return (
+    <div className={styles['review-tab']}>
+      <div className="container">
+        <Link
+          to={'/reviews'}
+          className={clsx({
+            [styles.actives]: !reviewPageIndex,
+          })}
+          onClick={() => onClick(1)}
+        >
+          Независимый рейтинг
+        </Link>
+        <Link
+          className={clsx({
+            [styles.actives]: +reviewPageIndex === 2,
+          })}
+          to="/reviews?tabIndex=2"
+          onClick={() => onClick(2)}
+        >
+          Интервью с пациентами
+        </Link>
+        <Link
+          className={clsx({
+            [styles.actives]: +reviewPageIndex === 3,
+          })}
+          to="/reviews?tabIndex=3"
+          onClick={() => onClick(3)}
+        >
+          Отзывы в социальных сетях
+        </Link>
+        <Link
+          className={clsx({
+            [styles.actives]: +reviewPageIndex === 4,
+          })}
+          to="/reviews?tabIndex=4"
+          onClick={() => onClick(4)}
+        >
+          Отзывы пациентов
+        </Link>
+      </div>
+    </div>
+  )
 }
--- src/widgets/common/Header/ui/HeaderTopSlice.jsx
+++ src/widgets/common/Header/ui/HeaderTopSlice.jsx
@@ -8,40 +8,40 @@ import styles from '../index.module.scss'
 import DropDown from '../../../../features/DropDown'
 
 export const HeaderTopSlice = () => {
-	const cityConfig = {
-		items: ['Москва1', 'Иркутск', 'Санкт-Петербург'],
-		initialSelectedItem: (
-			<>
-				Москва <IoIosArrowDown />
-			</>
-		)
-	}
+  const cityConfig = {
+    items: ['Москва1', 'Иркутск', 'Санкт-Петербург'],
+    initialSelectedItem: (
+      <>
+        Москва <IoIosArrowDown />
+      </>
+    ),
+  }
 
-	return (
-		<div className={styles['top-slice']}>
-			<div className={`${styles.container}`}>
-				<div className={styles.cursor}>
-					<img src={Cursor} alt='' />
-					<DropDown minWidth={170} {...cityConfig} />
-				</div>
-				<div className={`${styles.div} flex items-center gap-2`}>
-					<img src={TelIcon} alt='' />
-					<a href='tel:+8 (800) 200-45-65'>8 (800) 200-45-65</a>
-					<a href='tel:+8 (800) 200-45-65'>Заказать звонок</a>
-				</div>
-				<div className={`${styles.div} flex items-center gap-2`}>
-					<img src={ClockIcon} alt='' />
-					<span>В будни с 9:00 до 18:00</span>
-				</div>
-				<div className={`${styles.div} flex items-center gap-2`}>
-					<img src={EmailIcon} alt='' />
-					<a href='mailto:Consult@clinic.ru'>Consult@clinic.ru</a>
-				</div>
-				<div className={`${styles.div} flex items-center gap-2`}>
-					<img src={Glass} alt='' />
-					<span>Версия для слабовидящих</span>
-				</div>
-			</div>
-		</div>
-	)
+  return (
+    <div className={styles['top-slice']}>
+      <div className={`${styles.container}`}>
+        <div className={styles.cursor}>
+          <img src={Cursor} alt="" />
+          <DropDown minWidth={170} {...cityConfig} />
+        </div>
+        <div className={`${styles.div} flex items-center gap-2`}>
+          <img src={TelIcon} alt="" />
+          <a href="tel:+8 (800) 200-45-65">8 (800) 200-45-65</a>
+          <a href="tel:+8 (800) 200-45-65">Заказать звонок</a>
+        </div>
+        <div className={`${styles.div} flex items-center gap-2`}>
+          <img src={ClockIcon} alt="" />
+          <span>В будни с 9:00 до 18:00</span>
+        </div>
+        <div className={`${styles.div} flex items-center gap-2`}>
+          <img src={EmailIcon} alt="" />
+          <a href="mailto:Consult@clinic.ru">Consult@clinic.ru</a>
+        </div>
+        <button className={`${styles.div} flex items-center gap-2`}>
+          <img src={Glass} alt="" />
+          <span>Версия для слабовидящих</span>
+        </button>
+      </div>
+    </div>
+  )
 }
