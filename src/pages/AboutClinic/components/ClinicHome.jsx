33c23bdb4eeebdcf01631194c231ae0dde8cd729
aziret <kubaaziret68@gmail.com>
2024-03-25 19:35:11 +0600

    aziret finish deploy

--- src/pages/AboutClinic/components/ClinicHome.jsx
+++ src/pages/AboutClinic/components/ClinicHome.jsx
@@ -3,20 +3,20 @@ import Rating from '../../../widgets/common/Rating'
 import Social from '../../../widgets/common/Social'
 import Teeth from '../../../widgets/common/Teeth'
 import Equiment from '../../../widgets/Equipment'
-import WeUse from '../../../widgets/WeUse'
+import WeUse from '../../../widgets/WeUse/index'
 import WeUse1 from '../../../widgets/WeUse/WeUse1'
 
 function ClinicHomo() {
-	return (
-		<>
-			<Teeth />
-			<Rating />
-			<Clinic />
-			<Social />
-			<Equiment />
-			<WeUse1 />
-			<WeUse />
-		</>
-	)
+  return (
+    <>
+      <Teeth />
+      <Rating />
+      <Clinic />
+      <Social />
+      <Equiment />
+      <WeUse1 />
+      <WeUse />
+    </>
+  )
 }
 export default ClinicHomo
