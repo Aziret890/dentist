4ea5a5eb86a7f4e51e571f98b37fccc68688a8db
aziret <kubaaziret68@gmail.com>
2024-03-25 19:50:04 +0600

    my project

--- src/pages/AboutClinic/components/ClinicHome.jsx
+++ src/pages/AboutClinic/components/ClinicHome.jsx
@@ -3,8 +3,8 @@ import Rating from '../../../widgets/common/Rating'
 import Social from '../../../widgets/common/Social'
 import Teeth from '../../../widgets/common/Teeth'
 import Equiment from '../../../widgets/Equipment'
-import WeUse from '../../../widgets/WeUse/WeUse'
-import WeUse1 from '../../../widgets/WeUse/WeUse1'
+import WeUse from '../../../widgets/weUse/WeUse'
+import WeUse1 from '../../../widgets/weUse/WeUse1'
 
 function ClinicHomo() {
   return (
